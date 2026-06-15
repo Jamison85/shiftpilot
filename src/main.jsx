2f864e48ec64d43bfe94c16db7a17dd7221697de
Jamison85 <jamisonr85@gmail.com>
2026-06-15 12:52:58 -0500

    Load automatic timing styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Root from './Root.jsx';
 import './styles.css';
 import './timebudget.css';
+import './smarttime.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><Root /></React.StrictMode>
