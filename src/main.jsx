8c1a31f5f04373df4964ceade5d99431655413e0
Jamison85 <jamisonr85@gmail.com>
2026-06-15 12:36:27 -0500

    Load time budget styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from './Root.jsx';
 import './styles.css';
+import './timebudget.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><Root /></React.StrictMode>
