d19281e3e2966448c6490930ccd0d122f54beddf
Jamison85 <jamisonr85@gmail.com>
2026-06-15 22:40:55 -0500

    Load manager intelligence styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppV2 from './AppV2.jsx';
 import './appV2.css';
+import './managementV3.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><AppV2 /></React.StrictMode>
