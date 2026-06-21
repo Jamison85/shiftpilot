6818818b8002fa2d993c314533391179789a82a0
Jamison85 <jamisonr85@gmail.com>
2026-06-21 14:30:28 -0500

    Mount incident reporter

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './interruptionInputPatch.js';
 import './prepShiftPatch.js';
 import AppV2 from './AppV2.jsx';
+import IncidentReporter from './IncidentReporter.jsx';
 import './appV2.css';
 import './managementV3.css';
 import './premium-theme.css';
 import './finishPolish.css';
+import './incidentReporter.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><AppV2 /></React.StrictMode>
+  <React.StrictMode>
+    <AppV2 />
+    <IncidentReporter />
+  </React.StrictMode>
 );
 
 if ('serviceWorker' in navigator) {
