93261b5e198af413ee44cf371f92da65ada6b863
Jamison85 <jamisonr85@gmail.com>
2026-06-22 00:48:00 -0500

    Bump cache for clearer calm video

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='shiftpilot-v10-findtrail-visible-video';
+const CACHE='shiftpilot-v11-findtrail-clearer-video';
 const APP_SHELL=['/','/index.html','/findtrail.html','/manifest.webmanifest','/findtrail-manifest.webmanifest','/icon.svg','/findtrail-icon.svg','/calm-poster.svg'];
 
 self.addEventListener('install',event=>{
