7444594903b1ee606f90274974b793b369859a13
Jamison85 <jamisonr85@gmail.com>
2026-06-21 22:56:26 -0500

    Add FindTrail React entrypoint

---
+++ src/findtrail-main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FindTrailApp from './FindTrailApp.jsx'
+import './findtrail.css'
+
+ReactDOM.createRoot(document.getElementById('findtrail-root')).render(
+  <React.StrictMode>
+    <FindTrailApp />
+  </React.StrictMode>,
+)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {})
+  })
+}
